7a04da56b3616e54d4a1d300749d20dc8e5a96f5
LOWZIHONG <S10227747@connect.np.edu.sg>
2021-12-02 09:17:43 +0800

    Commit changes

--- Script.js
+++ Script.js
@@ -1,7 +1,11 @@
-const menu = document.querySelector('#mobile-menu');
-const menulinks = document.querySelector('.navbar__menu');
+/* This Javascript is for when user clicks it animates the dropdown Hamburger menu */
 
-menu.addEventListener('click', function() {
-  menu.classList.toggle('is-active');
-  menulinks.classList.toggle('active');
+const menu = document.querySelector('#mobile-menu'); /* This line of code targets the ID mobile-menu as seen in the navbar section */
+const menulinks = document.querySelector('.navbar__menu'); /* This line of code targets the ID navbar-menu as seen in the navbar section */
+
+menu.addEventListener('click', function() { /* This line of code is to create a function when the user performs the action 'Click' */
+  menu.classList.toggle('is-active'); /* This line of code  toggles and adds a classlist 'is-active' both classes are used in Style CSS */
+  menulinks.classList.toggle('active'); /* This line of code  toggles and adds a classlist 'active' both classes are used in Style CSS*/
 });
+
+/* Reference from https://developer.mozilla.org/en-US/docs/Web/API/DOMTokenList/toggle*/
